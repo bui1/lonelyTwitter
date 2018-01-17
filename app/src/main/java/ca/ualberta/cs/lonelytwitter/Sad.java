f9042277fb2aaf394b422eb6db265e13a9e83469
Monica Bui <bui1@ualberta.ca>
2018-01-16 21:34:58 -0700

    Create Sad.java

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Sad.java
@@ -0,0 +1,22 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+/**
+ * Created by bui1 on 1/16/18.
+ */
+
+public class Sad extends MainMood {
+    Sad(String mood){
+        super(mood);
+    }
+
+    Sad(String mood, Date date){
+        super(mood,date);
+    }
+
+    @Override
+    public Boolean isFeelingGood(){
+        return Boolean.False;
+    }
+}
