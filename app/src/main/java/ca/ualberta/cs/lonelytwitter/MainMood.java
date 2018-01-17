2f6180a11d0ebbe8354a78e5136012c16874d969
Monica Bui <bui1@ualberta.ca>
2018-01-16 21:38:14 -0700

    fixed error

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/MainMood.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/MainMood.java
@@ -1,4 +1,10 @@
+package ca.ualberta.cs.lonelytwitter;
 
+import java.util.Date;
+
+/**
+ * Created by bui1 on 1/16/18.
+ */
 
 public abstract class MainMood(){
 	private String mood; // private variables
