88d0b796022f82d4cf6678ce2f86885ba6063576
Monica Bui <bui1@ualberta.ca>
2018-01-16 21:26:11 -0700

    created main class for mood

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/MainMood.java
@@ -0,0 +1,41 @@
+
+
+public abstract class MainMood(){
+	private String mood; // private variables
+	private Date date;
+	
+	MainMood(String mood){
+		this.mood = mood;
+		date = new Date();
+	}
+	
+	MainMood(String mood, Date date){
+		this.mood = mood;
+		this.date = date;
+	}
+	
+	public void getMood(){ return mood;}
+	
+	public void setMood(String mood) throws MoodException{
+        if (mood == "happy"){
+            this.mood = "happy";
+        }
+		else if (mood == "sad"){
+			this.mood = "sad";
+		}
+		else if (mood == "mad"){
+			this.mood = "mad";
+		}
+        else{
+            throw new MoodException();
+        }
+    }
+
+    public Date getDate() { return date;}
+
+
+
+    public abstract Boolean isImportant();
+	
+	
+}
